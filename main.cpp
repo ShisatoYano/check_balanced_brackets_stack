63c9c790f94121d0387646d597b26ebd04412ac4
Shisato <shisatoyano@gmail.com>
2020-12-26 17:38:21 +0900

    implemented check balanced brackets using stack

--- main.cpp
+++ main.cpp
@@ -1,6 +1,74 @@
-#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+// check if brackets are balanced
+bool are_brackets_balanced(string input) {
+    stack<char> s;
+    char x;
+
+    // traversing
+    for (char & i : input) {
+        if ((i == '(') ||
+            (i == '[') ||
+            (i == '{')) {
+            s.push(i);
+            continue;
+        }
+
+        // current char is not opening bracket,
+        // it must be closing. stack cannot be empty
+        if (s.empty()) {
+            return false;
+        }
+
+        switch (i) {
+            case ')':
+                x = s.top();
+                s.pop();
+                if (x == '{' || x == '[') {
+                    return false;
+                }
+                break;
+            case '}':
+                x = s.top();
+                s.pop();
+                if (x == '(' || x == '[') {
+                    return false;
+                }
+                break;
+            case ']':
+                x = s.top();
+                s.pop();
+                if (x == '(' || x == '{') {
+                    return false;
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    return s.empty();
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    string input_balanced = "{()}[]";
+
+    cout << input_balanced << " is ";
+    if (are_brackets_balanced(input_balanced)) {
+        cout << "Balanced" << endl;
+    } else {
+        cout << "Not Balanced" << endl;
+    }
+
+    string input_unbalanced = "[(])";
+
+    cout << input_unbalanced << " is ";
+    if (are_brackets_balanced(input_unbalanced)) {
+        cout << "Balanced" << endl;
+    } else {
+        cout << "Not Balanced" << endl;
+    }
+
     return 0;
 }
